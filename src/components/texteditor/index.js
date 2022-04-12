10f919ba51fa87bb60a33c39b07b4ea93ac969d1
sahir-anas <anas.sahir-etu@etu.univh2c.ma>
2022-04-12 00:55:17 +0000

    init

--- src/components/texteditor/index.js
+++ src/components/texteditor/index.js
@@ -11,20 +11,7 @@ export default function TextEditor() {
     const [body, setbody] = useState("");
     function handleBody(e) {
         setbody(e);
-        // const blocksFromHtml = htmlToDraft(body);
-        // console.log("blocksFromHtml: " + blocksFromHtml);
-        // const { contentBlocks, entityMap } = blocksFromHtml;
-        // const contentState = ContentState.createFromBlockArray(contentBlocks, entityMap);
-        // const editorState = EditorState.createWithContent(contentState);
-        // getText(e);
     }
-
-    // function getText(html) {
-    //     const text = convert(html, {
-    //         wordwrap: 130
-    //     });
-    //     // console.log(text);
-    // }
 
     return (
         <>
@@ -33,17 +20,7 @@ export default function TextEditor() {
                 modules={TextEditor.modules}
                 formats={TextEditor.formats}
                 onChange={handleBody}
-                value={body}
             />
-
-            {body}
-            {" "}
-            < div dangerouslySetInnerHTML={{ __html: body }} />
-            {/* <div dangerouslySetInnerHTML={{ __html: "<h1 style='font-size:50px'><strong>hy there</strong></h1><h2><em>first things first</em></h2><p>ana db gaaaa kanjereb fel editor wach i9der ikhdem liya html west JSX.</p><p>hado homa lmachakil li l9ithom db :</p><ul><li>header makhdamch</li><li>size makhdamch</li><li>bold et italic makhdaminch</li><li>o 7ta had la3ba li 3la <s>hadi</s> makhdamach</li></ul><p><strong class='ql- font - monospace ql-size-large'>idan chno khasna ndiro ????</strong></p><p>ana gelt nkteb blog li howa hada nit o njereb zahri hhhhhhhh</p>" }} /> */}
-            {/* < div dangerouslySetInnerHTML={{ __html: "<h1 style='font-size:20px'><span style = 'color:hotpink'>keep me and make me pretty!</span></h1 > " } */}
-            {/* {ReactHtmlParser(body)} */}
-            {/* {ReactHtmlParser("<h1 style='font-size:20px'><span style = 'color:hotpink'>keep me and make me pretty!</span></h1 > ")} */}
-            {/* {body} */}
         </>
 
     );
